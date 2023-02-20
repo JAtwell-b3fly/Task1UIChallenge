12187b4bf40e0800832facf99cef9ca1f8c6a389
JoshuaATMartin <100522979+JoshuaATMartin@users.noreply.github.com>
2023-02-20 13:49:25 +0200

    "created header and footer templates

---
+++ src/Header Template.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+// Function 
+const headerTemplate = () => {
+  return (
+    <View id = 'headerContainer'>
+
+    </View>
+  );
+};
+
+// Styling
+const headerStyle = StyleSheet.create({
+
+ container:{
+    color: 'light blue'
+
+ }
+
+
+
+})
