45bffd5b8d2aa00d0109bd09c85dc8ab0b668c4e
supportjoxicraft <support@joxicraft.co.za>
2023-03-09 10:51:04 +0200

    New Folders and files Created

--- src/View/Components/Sub-Components/Common/LoginButton.js
+++
@@ -1,8 +0,0 @@
-import { Button, Box } from "native-base";
-
-const Example = () => {
-  return <Box alignItems="center">
-      <Button onPress={() => console.log('')} size="md" >Login</Button>
-    </Box>;
-};
-export default Example;
