88e650623a5fe383519592fa0f3f993dddaf4ed0
JozCapption <125558537+JozCapption@users.noreply.github.com>
2023-03-15 09:50:02 +0200

    Update ProductDetailsScreen.js

--- src/Screens/ProductDetailsScreen.js
+++ src/Screens/ProductDetailsScreen.js
@@ -1,78 +1,35 @@
-// Import necessary components from external files
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import RatingForm from './components/RatingForm.js';
-import CatalogBox from './components/CatalogBox.js';
+import React, { useState, useEffect } from "react";
+import ProductHeader from "./ProductHeader";
+import ProductGallery from "./ProductGallery";
+import ProductTitle from "./ProductTitle";
+import ProductRating from "./ProductRating";
+import ProductDescription from "./ProductDescription";
+import RatingAndReviews from "./RatingAndReviews";
+import RelatedProducts from "./RelatedProducts";
+import ProductFooter from "./ProductFooter";
+import "./productDescription.css";
 
-// Get product details from the database
-const product = getProductDetails(productId);
+const ProductDescription = ({ productId }) => {
+  const [product, setProduct] = useState({});
 
-// Create the necessary elements for the screen
-const header = new Header();
-const footer = new Footer();
-const ratingForm = new RatingForm();
-const catalogBox = new CatalogBox();
+  useEffect(() => {
+    fetch(`https://api.example.com/products/${productId}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data));
+  }, [productId]);
 
-const productImage = document.createElement('img');
-productImage.src = product.image;
+  return (
+    <div>
+      <ProductHeader />
+      <ProductGallery images={product.images} />
+      <ProductTitle title={product.title} />
+      <ProductRating rating={product.rating} price={product.price} />
+      <ProductDescription description={product.description} />
+      <RatingAndReviews />
+      <RelatedProducts relatedProducts={product.relatedProducts} />
+      <ProductFooter />
+    </div>
+  );
+};
 
-const backButton = document.createElement('button');
-backButton.textContent = 'Back';
-backButton.addEventListener('click', goBack);
-
-const addToCartButton = document.createElement('button');
-addToCartButton.textContent = 'Add to Cart';
-addToCartButton.addEventListener('click', addToCart);
-
-const addToWishcartButton = document.createElement('button');
-addToWishcartButton.textContent = 'Add to Wishlist';
-addToWishcartButton.addEventListener('click', addToWishlist);
-
-const title = document.createElement('h1');
-title.textContent = product.title;
-
-const averageRating = document.createElement('p');
-averageRating.textContent = `Average Rating: ${product.averageRating}`;
-
-const price = document.createElement('p');
-price.textContent = `$${product.price}`;
-
-const description = document.createElement('p');
-description.textContent = product.description;
-
-const ratingsAndReviewsHeading = document.createElement('h2');
-ratingsAndReviewsHeading.textContent = 'Ratings and Reviews';
-
-// Add the elements to the screen
-const container = document.querySelector('#product-description-screen');
-container.appendChild(header.render());
-container.appendChild(productImage);
-container.appendChild(backButton);
-container.appendChild(addToCartButton);
-container.appendChild(addToWishcartButton);
-container.appendChild(title);
-container.appendChild(averageRating);
-container.appendChild(price);
-container.appendChild(description);
-container.appendChild(ratingsAndReviewsHeading);
-container.appendChild(ratingForm.render());
-container.appendChild(catalogBox.render());
-container.appendChild(footer.render());
-
-// Implement necessary functions
-function getProductDetails(productId) {
-  // Use AJAX to fetch product details from the server
-  // and return the result as an object
-}
-
-function goBack() {
-  // Implement logic for navigating back to the previous screen
-}
-
-function addToCart() {
-  // Implement logic for adding the product to the user's cart
-}
-
-function addToWishlist() {
-  // Implement logic for adding the product to the user's wishlist
-}
+export default ProductDescription;
