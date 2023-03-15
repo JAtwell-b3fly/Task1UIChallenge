9ccc629ea701cc5851bd8ae773ee6de58eef5ea3
smereeotlhe <relebohilehlabana96@gmail.com>
2023-03-15 10:52:37 +0200

    modified  firebase config file aborts

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,7 +1,5 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
-import * as firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const firebaseConfig = {
   // Your Firebase configuration object
