7ca5d655c5a4f090fe4bb2b25cb75ce27ba8dafa
JozCapption <125558537+JozCapption@users.noreply.github.com>
2023-03-15 10:33:07 +0200

    CartScreen & ProductDetailsScreen added

---
+++ src/Screens/CartScreen.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import ProductCart from "./ProductCart";
+
+function ShoppingCart() {
+  // Get cart data from database using API management tool
+  const cartData = [];
+
+  // Calculate total values based on cart data
+  const subTotal = 0;
+  const tax = 0;
+  const total = 0;
+
+  // Function to handle removing a product from the cart
+  function handleRemoveProduct(productId) {
+    // Update cart data in the database using API management tool
+  }
+
+  // Function to handle navigating back to the previous screen
+  function handleBackNavigation() {
+    // Navigate to previous screen using React Router or similar library
+  }
+
+  // Function to handle navigating to the checkout screen
+  function handleCheckoutNavigation() {
+    // Navigate to checkout screen using React Router or similar library
+  }
+
+  return (
+    <div>
+      <Header />
+      <div>
+        <button onClick={handleBackNavigation}>Back</button>
+        <h1>Shopping Cart</h1>
+        <div>
+          {cartData.map((product) => (
+            <ProductCart
+              key={product.id}
+              product={product}
+              onRemove={handleRemoveProduct}
+            />
+          ))}
+        </div>
+        <div>
+          <p>Sub-Total:</p>
+          <p>{subTotal}</p>
+        </div>
+        <div>
+          <p>Tax:</p>
+          <p>{tax}</p>
+        </div>
+        <div>
+          <p>Total:</p>
+          <p>{total}</p>
+        </div>
+        <div>
+          <button onClick={handleCheckoutNavigation}>Checkout</button>
+          <button>Continue Shopping</button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default ShoppingCart;
--- src/Screens/ProductDetailsScreen.js
+++ src/Screens/ProductDetailsScreen.js
@@ -1,35 +1,72 @@
-import React, { useState, useEffect } from "react";
-import ProductHeader from "./ProductHeader";
-import ProductGallery from "./ProductGallery";
-import ProductTitle from "./ProductTitle";
-import ProductRating from "./ProductRating";
-import ProductDescription from "./ProductDescription";
-import RatingAndReviews from "./RatingAndReviews";
-import RelatedProducts from "./RelatedProducts";
-import ProductFooter from "./ProductFooter";
-import "./productDescription.css";
+import React, { useState, useEffect } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProductImageGallery from './components/ProductImageGallery';
+import ProductTitle from './components/ProductTitle';
+import ProductRating from './components/ProductRating';
+import ProductPrice from './components/ProductPrice';
+import ProductDescription from './components/ProductDescription';
+import RatingForm from './components/RatingForm';
+import RelatedProducts from './components/RelatedProducts';
 
-const ProductDescription = ({ productId }) => {
+const ProductDescriptionScreen = (props) => {
   const [product, setProduct] = useState({});
+  const productId = props.match.params.id; // Get product ID from URL
 
   useEffect(() => {
-    fetch(`https://api.example.com/products/${productId}`)
-      .then((response) => response.json())
-      .then((data) => setProduct(data));
+    // Fetch product data from backend API based on ID
+    // Update product state
   }, [productId]);
 
+  const handleRatingSubmit = (ratingData) => {
+    // Send rating data to backend API
+    // Update product rating state
+  };
+
+// Get the product ID from the URL
+//const urlParams = new URLSearchParams(window.location.search);
+//const productId = urlParams.get('productId');
+
+// Fetch the product data from the database using the product ID
+//fetch(`/api/products/${productId}`)
+//  .then(response => response.json())
+//  .then(product => {
+    // Use the product data to populate the screen components
+    //const productTitle = document.getElementById('product-title');
+    //productTitle.textContent = product.title;
+
+    //const productImage = document.getElementById('product-image');
+    //productImage.src = product.image;
+
+    //const productPrice = document.getElementById('product-price');
+    //productPrice.textContent = `$${product.price}`;
+
+    // ...
+//  })
+//  .catch(error => {
+//    console.error(error);
+//  });
+
   return (
-    <div>
-      <ProductHeader />
-      <ProductGallery images={product.images} />
-      <ProductTitle title={product.title} />
-      <ProductRating rating={product.rating} price={product.price} />
-      <ProductDescription description={product.description} />
-      <RatingAndReviews />
-      <RelatedProducts relatedProducts={product.relatedProducts} />
-      <ProductFooter />
+    <div className="product-description-screen">
+      <Header />
+      <main>
+        <ProductImageGallery images={product.images} />
+        <div className="product-details">
+          <ProductTitle title={product.title} />
+          <ProductRating rating={product.rating} />
+          <ProductPrice price={product.price} />
+          <ProductDescription description={product.description} />
+          <div className="ratings-reviews">
+            <h2>Ratings and Reviews</h2>
+            <RatingForm onRatingSubmit={handleRatingSubmit} />
+          </div>
+          <RelatedProducts products={product.relatedProducts} />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 };
 
-export default ProductDescription;
+export default ProductDescriptionScreen;
