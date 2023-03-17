41b700984cd87ec47a1969b7239bd2dd19de6c13
JoshuaATMartin <100522979+JoshuaATMartin@users.noreply.github.com>
2023-03-17 16:30:17 +0200

    header component update  17/03/23

--- src/Layouts/MainLayout.js
+++ src/Layouts/MainLayout.js
@@ -4,13 +4,13 @@ import {View,Box,Text} from "native-base";
 
 
 <View>
-            <Box width = "100%" height="100%" bgColor={"lightBlue.100"} safeArea  >
-                <VStack>
-                    <Box  width = "100%" height="15%" bg={"yellow.500"}>
+            <Box  width = "100%" height = "100%" bgColor={"lightBlue.100"} safeArea  >
+                <VStack flex = {1}>
+                    <Box  width = "100%" flex = {2} bg={"yellow.500"}>
                         <Text>HEADER SPACE</Text>
                     </Box>
 
-                    <Box width = "100%" height="85%" bg={"red.900"}>
+                    <Box  width = "100%" flex = {8} bg={"red.900"}>
                         <Text>BODY SPACE</Text>
                     </Box>
 
--- src/components/subcomponents/common/Input.js
+++ src/components/subcomponents/common/Input.js
@@ -1,12 +1,13 @@
 import { Input } from "native-base";
 
 const MInput = (props) => {
-    const {width,placeHolder,size,type,isReadOnly,isInvalid,inputLeft,inputRight,isRequired,inFocusColor,isInvalidColor} = props
+    const {bgColor,flex,h,my,onChange,width,variant,placeHolder,size,type,isReadOnly,isInvalid,inputLeft,inputRight,isRequired,inFocusColor,isInvalidColor} = props
     return (
 
-        <Input w = {width} placeholder = {placeHolder} size = {size} type = {type} isReadOnly = {isReadOnly} isInvalid = {isInvalid} 
+        <Input flex = {flex} h={h} my = {my} onChangeText={onChange} w = {width} placeholder = {placeHolder} size = {size} type = {type} isReadOnly = {isReadOnly} isInvalid = {isInvalid} 
         
-            leftElement = {inputLeft} rightElement = {inputRight} isRequired = {isRequired} focusOutlineColor = {inFocusColor} invalidOutlineColor = {isInvalidColor}/>
+            InputLeftElement = {inputLeft} InputRightElement = {inputRight} isRequired = {isRequired} focusOutlineColor = {inFocusColor} invalidOutlineColor = {isInvalidColor}
+            variant = {variant} bgColor = {bgColor}/>
         
         
     );
