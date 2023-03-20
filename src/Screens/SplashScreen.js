acb430b6e25fe78e9fe38ebbe96812e8d0bbdb26
Sabata Mereeotlhe <sabata@joxicraft.co.za>
2023-03-20 18:37:57 +0200

    Co-authored-by: JozCapption <JozCapption@users.noreply.github.com> Co-authored-by: JoshAtCapption <JoshAtCapption@users.noreply.github.com>

--- src/Screens/SplashScreen.js
+++ src/Screens/SplashScreen.js
@@ -11,7 +11,7 @@ const SplashScreen = ({ navigation }) => {
   useEffect(() => {
     // Navigate to the login screen after 5 seconds
     setTimeout(() => {
-      navigation.replace('Login');
+      navigation.replace('Login'); TODO: `Don't lead the user straigh to login but rather into the app to immedlately purchase`
     }, 5000);
   }, []);
 
---
+++ src/components/productCategories.js
@@ -0,0 +1,6 @@
+
+function productCategories()
+{
+
+}
+export default {productCategories, } 
