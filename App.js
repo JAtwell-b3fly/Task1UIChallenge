92a44d724b5a5759dfc60c4805e68cd34d5c881d
JozCapption <125558537+JozCapption@users.noreply.github.com>
2023-03-24 15:59:42 +0200

    Added the Login Test Screen to the App.js
    Added the Login Screen to the App.js

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 
 import { NativeBaseProvider} from 'native-base';
 import Navigation from './src/navigationContainer';
+import LoginTest from './src/Screens/LoginTestScreen';
 export default function App() {
   return (
     <NativeBaseProvider>
       
       <Navigation/>
+      <LoginTest/>
          
     </NativeBaseProvider>
     
