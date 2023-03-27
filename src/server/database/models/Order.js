9128942ff74995480ddeab8d19fc4ba1db750705
smereeotlhe <relebohilehlabana96@gmail.com>
2023-03-27 09:58:43 +0200

    deleted items

--- src/server/database/models/Order.js
+++
@@ -1,8 +0,0 @@
-import { db } from '../firebase';
-
-const ordersRef = db.collection('orders');
-
-export const createOrder = async (order) => {
-    const orderRef = await ordersRef.add(order);
-    return orderRef.id;
-};
