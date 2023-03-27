4c3d3fb717e7ef26fad04c1cea18b9a7760d2ea7
smereeotlhe <relebohilehlabana96@gmail.com>
2023-03-27 09:53:24 +0200

    server changes

---
+++ src/server/models/chemicalproduct.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ChemicalProduct extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ChemicalProduct.init({
+    name: DataTypes.STRING,
+    description: DataTypes.STRING,
+    ingredients: DataTypes.STRING,
+    usageInstructions: DataTypes.STRING,
+    safetyPrecautions: DataTypes.STRING,
+    imageUrl: DataTypes.STRING,
+    rating: DataTypes.FLOAT,
+    numOfReviews: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ChemicalProduct',
+  });
+  return ChemicalProduct;
+};
