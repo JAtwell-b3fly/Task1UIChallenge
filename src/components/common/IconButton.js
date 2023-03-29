e9d2e6c2e88dc559442042a57b46ca751917b362
JoshuaATMartin <100522979+JoshuaATMartin@users.noreply.github.com>
2023-03-29 14:34:56 +0200

    Product Item complete and installed star Rating

--- src/components/common/IconButton.js
+++ src/components/common/IconButton.js
@@ -2,5 +2,9 @@ import {IconButton,Icon} from 'native-base';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 export const CIconButton = ({myIcon,onPress}) =>
 {
-    <IconButton  icon={<Icon as={MaterialCommunityIcons} name={myIcon} />} borderRadius="full"/>
+    return(
+
+        <IconButton  icon={<Icon as={MaterialCommunityIcons} name={myIcon} />} borderRadius="full"/>
+    );
+    
 }
