dbbb978572d2498cac54b7da955429e27b9d579f
smereeotlhe <relebohilehlabana96@gmail.com>
2023-04-05 14:33:37 +0200

    Finder updates for refundpolicy and session

--- src/server/models/session.js
+++ src/server/models/session.js
@@ -12,16 +12,54 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
+    // Finder methods
+    static findAllSessions() {
+      // findAllSessions(): returns all sessions.
+      return Session.findAll();
+    }
+  
+    static findSessionById(id) {
+      // findSessionById(id): returns the session with the specified id.
+      return Session.findByPk(id);
+    }
+  
+    static createSession(session) {
+      // createSession(session): creates a new session with the specified data.
+      return Session.create(session);
+    }
+  
+    static updateSession(id, session) {
+      // updateSession(id, session): updates the session with the specified
+      // id with the specified data.
+      return Session.update(session, {
+        where: {
+          id
+        }
+      });
+    }
+  
+    static destroySession(id) {
+      // destroySession(id): deletes the session with the specified id.
+      return Session.destroy({
+        where: {
+          id
+        }
+      });
+    }
   }
   Session.init({
+    // Model attributes are defined here
     user_id: DataTypes.INTEGER,
     session_data: DataTypes.TEXT,
     token: DataTypes.STRING,
     created_at: DataTypes.DATE,
     updated_at: DataTypes.DATE
   }, {
-    sequelize,
-    modelName: 'Session',
+    sequelize, // We need to pass the connection instance
+    modelName: 'Session',// We need to choose the model name
+    tableName: 'Sessions',// We need to choose the table  name
+    timestamps: true,
+    underscored: true
   });
   return Session;
 };
