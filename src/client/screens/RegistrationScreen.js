55c7ae8f934298e3b59c6bf5507af4e6bbd15106
JozCapption <125558537+JozCapption@users.noreply.github.com>
2023-04-13 10:40:01 +0200

    Added Actions and Navigation to the RegistrationScreen.js
    Added Actions and Navigation to the RegistrationScreen.js. The screen still needs code to set avatar, to send data to the database.

--- src/client/screens/RegistrationScreen.js
+++ src/client/screens/RegistrationScreen.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { Container, VStack, HStack, Input, Button, Text, Link, FormControl, Select, Avatar } from "native-base";
 
 //import the external files
 import Avatar from './components/Avatar';
@@ -13,28 +14,48 @@ import RegistrationForm from '../../components/Forms/RegistrationForm';
 import styles from './RegistrationStyles'; //import the custom stylesheet
 import { Group1Layout } from './Layouts/Group1Layout';
 
-//Actions
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [firstName, setFirstName] = useState('');
-const [lastName, setLastName] = useState('');
-const [gender, setGender] = useState('male');
+const RegistrationScreen = () => {
 
-const handleCreateAccount = () => {
-  // TODO: submit form data to the database
-  navigation.navigate('Login');
-};
+  //state variables to store the user input data
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const [firstName,setFirstName] = useState("");
+  const [lastName,setLastName] = useState("");
+  const [avatar,setAvatar] = useState("");
 
-export default function RegistrationScreen () {
+  //Action to submit form data to the database
+  const handleCreateAccount = () => {
+    //*** Code to submit user data to the database goes here ********
+
+    //navigate back to Login Screen
+    NavigationPreloadManager.navigate("LoginScreen.js");
+  };
+
+  //Navigation to Login Screen
+  const AlreadyHaveAnAccountNavigation = () => {
+    //disregard any data entered into form
+    setEmail("");
+    setPassword("");
+    setFirstName("");
+    setLastName("");
+    setAvatar("null");
+
+    //navigate to Login Screen
+    NavigationPreloadManager.navigate("LoginScreen.js");
+  };
+
+  //Action to set Avatar icon
+  const handleSetAvatar = () => {
+    //****Set user's avatar icon code comes here *******
+
+  };
 
   return (
     <Group1Layout applogoheader={<Logo />}
                   avatar={<Avatar />}
-<<<<<<< Updated upstream
-                  registrationSection={<RegistrationForm/>}
-=======
                   registrationSection={<RegistrationForm />}
->>>>>>> Stashed changes
     />
   );
 };
+
+export default RegistrationScreen;
