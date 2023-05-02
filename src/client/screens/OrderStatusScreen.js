c2301846c33e2d0056eeb503bff352fecabbb249
JozCapption <125558537+JozCapption@users.noreply.github.com>
2023-05-02 14:11:53 +0200

    Added Actions and Navigation to the OrderStatusScreen,js
    Added Actions and Navigation to the OrderStatusScreen.js. Such as generating and storing the order number, displaying the order number, order date, order status, navigating the user to the HomeScreen.js, OrderScreen, PaymentScreen.js

--- src/client/screens/OrderStatusScreen.js
+++ src/client/screens/OrderStatusScreen.js
@@ -1,12 +1,72 @@
-import React from "react";
-import { View } from "native-base";
+import React, {useEffect, useState} from "react";
+import { View, Text, Button } from "native-base";
+import { useNavigation } from '@react-navigation/native';
+import { db } from '../config/firebase';
+import { v4 as uuidv4 } from 'uuid';
 
 import { SecondHeaderLayout } from "../components/Layouts/HeaderOther";
 import { CLabel } from "../components/common/Label";
 import Group7Layout from "./Layouts/Group7Layout";
 import OrderStatusDetails from "../components/Layouts/OrderStatusDetails";
 
-export default function OrderStatus () {
+const OrderStatus = ({ navigation, route }) => {
+
+    //Set the state of each variable
+    const [orderNumber, setOrderNumber] = useState('');
+    const [orderStatus, setOrderStatus] = useState('Pending');
+    const [orderTotal, setOrderTotal] = useState(0);
+    const [orderDate, setOrderDate] = useState('');
+
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    useEffect(() => {
+        if (route.params) {
+          const { orderNumber, orderTotal, orderDate } = route.params;
+          setOrderNumber(orderNumber);
+          setOrderTotal(orderTotal);
+          setOrderDate(orderDate);
+        }
+      }, [route.params]);
+    
+      useEffect(() => {
+        setTotalAmount(route.params.totalAmount);
+        generateOrderNumber();
+      }, [route.params.totalAmount]);
+    
+      const generateOrderNumber = async () => {
+        let orderNumber = '';
+    
+        do {
+          orderNumber = uuidv4().substr(0, 6);
+    
+          const snapshot = await db.collection('orders').where('orderNumber', '==', orderNumber).get();
+          if (snapshot.size === 0) {
+            break;
+          }
+        } while (true);
+    
+        setOrderNumber(orderNumber);
+    
+        // Save order number to customer's order history and user account
+        // ... code to save order number ...
+      };
+
+  // Function to navigate to the Home screen
+  const navigateToHome = () => {
+    navigation.navigate('HomeScreen.js');
+  };
+
+  // Function to navigate to the Orders screen
+  const navigateToOrders = () => {
+    navigation.navigate('OrderScreen.js');
+  };
+
+  // Function to navigate to the Payment screen
+  const navigateToPayment = () => {
+    navigation.navigate('PaymentScreen.js');
+  };
+
     return(
         <View>
             <Group7Layout headercomponent2={<SecondHeaderLayout />}
@@ -16,3 +76,5 @@ export default function OrderStatus () {
         </View>
     )
 }
+
+export default OrderStatus;
